c95100172d32866eba1e254ab43774d54bcaa62d
guelaang <101912449+guelaang@users.noreply.github.com>
2024-10-02 09:19:11 +0700

    Update Tugas.py

--- MODUL_4/Tugas.py
+++ MODUL_4/Tugas.py
@@ -16,7 +16,18 @@ class UserService:
             return True
         return False
     
-   
+    def register(self, username, password):
+        if username in self.users:
+            print("Username already exists. Please try another one.")
+        elif not self.is_strong_password(password):
+            print(
+                "Password is not strong enough. It must be at least 8 characters long, "
+                "contain at least one uppercase letter, one lowercase letter, one number, "
+                "and one special character (@#$%^&+=!)."
+            )
+        else:
+            self.users[username] = password
+            print("Registration successful.")
 
     def login(self, username, password):
         if username in self.users and self.users[username] == password:
